7279d73388ac18a1da683de3bd9ba2b10dfca05d
Andrew Clark <andrewclark.ku@gmail.com>
2016-12-22 22:27:34 -0600

    removed negative positive on send button

--- js/contact_me.js
+++ js/contact_me.js
@@ -27,24 +27,29 @@ $(function() {
                     name: name,
                     phone: phone,
                     email: email,
-                    message: message
+                    message: message,
+            _gotcha: gotcha
                 },
+        dataType: "json",
                 cache: false,
-                success: function() {
+                success: function(data) {
                     // Enable button & show success message
-                    $("#btnSubmit").attr("disabled", false);
                     $('#success').html("<div class='alert alert-success'>");
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>")
+                     $('#success > .alert-success')
+                        .append(firstName)
+                    $('#success > .alert-success')
+                        .append(", thank you for your message.</strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
                     //clear all fields
                     $('#contactForm').trigger("reset");
                 },
-                error: function() {
+                error: function(err) {
                     // Fail message
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
